4d3a92df98f9e8a7011b39da2606844697615c9d
Mikkel <mikkelmadsen97@protonmail.com>
2022-10-05 21:26:05 +0200

    Added the following to SearchMethods.java: 1) SequentialSearch method 2) JumpSearch method 3) BinarySearch method

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,65 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Integer intarr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        // create integer for the index number
+        Integer indexOfElements;
 
-        List<String> Names = new ArrayList<String>(){};
+        // create integer array containing 10 numbers
+        Integer[] intarr = {10, 7, 8, 9, 5, 4, 3, 11, 2, 1, 6};
+
+        // create a list of names, containing 6 names
+        List<String> Names = new ArrayList<>(){};
         Names.add("Albert");
         Names.add("Robert");
         Names.add("Ulla");
         Names.add("Aske");
         Names.add("Berta");
-        Names.add("Karen");
 
-        Integer indexOfElements;
-
+        // Use SequentialSearch to find the index number at which the object is at
         indexOfElements = SearchMethods.SequentialSearch(intarr, 11);
         if (indexOfElements != null){
-            System.out.println("in intarr 11 is at index " + indexOfElements + " and we retrieved " + intarr[indexOfElements]);
+            System.out.println("Sequential Search (array): we found index " + indexOfElements + " and we retrieved " + intarr[indexOfElements]);
         }
 
+        // Use SequentialSearch to find the index number at which the object is at
         indexOfElements = SearchMethods.SequentialSearch(Names, "Ulla");
         if (indexOfElements != null){
-            System.out.println("in Names Ulla is at index " + indexOfElements + " and we retrieved " + Names.get(indexOfElements));
+            System.out.println("Sequential Search (list): we found index " + indexOfElements + " and we retrieved " + Names.get(indexOfElements));
         }
 
+        // Make new lists for JumpSearch and BinarySearch methods
+        Integer[] sortedIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 42};
 
+        List<String> SortedNames = new ArrayList<>();
+        SortedNames.add("Albert");
+        SortedNames.add("Aske");
+        SortedNames.add("Berta");
+        SortedNames.add("Robert");
+        SortedNames.add("Ulla");
+
+
+        indexOfElements = SearchMethods.JumpSearch(sortedIntArray, 11);
+        if (indexOfElements != null){
+            System.out.println("Jump Search (array): we found index " + indexOfElements + " and we retrieved " + sortedIntArray[indexOfElements]);
+        }
+
+        indexOfElements = SearchMethods.JumpSearch(SortedNames, "Ulla");
+        if (indexOfElements != null){
+            System.out.println("Jump Search (list): we found index " + indexOfElements + " and we retrieved " + SortedNames.get(indexOfElements));
+        }
+
+        indexOfElements = SearchMethods.BinarySearch(sortedIntArray, 11, 0, intarr.length - 1);
+        if (indexOfElements != null){
+            System.out.println("Binary Search (array): we found index " + indexOfElements + " and we retrieved " + sortedIntArray[indexOfElements]);
+        } else {
+            System.out.println("Number not found.");
+        }
+
+        indexOfElements = SearchMethods.BinarySearch(SortedNames, "Ulla", 0, Names.size() - 1);
+        if (indexOfElements != null){
+            System.out.println("Binary Search (list): we found index " + indexOfElements + " and we retrieved " + SortedNames.get(indexOfElements));
+        } else {
+            System.out.println("Name not found.");
+        }
     }
 }
--- src/SearchMethods.java
+++ src/SearchMethods.java
@@ -4,20 +4,175 @@ class SearchMethods {
 
     // SEQUENTIAL SEARCH
     public static <T extends Comparable<T>> Integer SequentialSearch(T[] list, T object){
+        for (int i = 0; i < list.length; i++){
+            if (list[i].compareTo(object) == 0){
+                return i;
+            }
+        }
         return null;
     }
 
     public static <T extends Comparable<T>> Integer SequentialSearch(List<T> list, T object){
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).compareTo(object) == 0){
+                return i;
+            }
+        }
         return null;
     }
 
     // JUMP SEARCH
-
     public static <T extends Comparable<T>> Integer JumpSearch(T[] list, T object){
+        // calculate the squareroot of the list.length
+        int squareRootOfListLength = (int) Math.sqrt(list.length);
+        // create an integer for the last position jumped to
+        int previousPosition = 0;
+
+        // I have no clue what the fuck is going on here - apparently it's a jumpsearch
+        while (list[Math.min(squareRootOfListLength - 1, list.length - 1)].compareTo(object) < 0){
+            // set the current position to the previous position
+            previousPosition = squareRootOfListLength;
+
+            // basically do square root + square root
+            squareRootOfListLength = squareRootOfListLength + (int) Math.sqrt(list.length);
+
+            // to avoid throwing an exception and ending the program, just return null if previousPosition is equal to
+            // or longer than the length of the list.
+            if (previousPosition >= list.length){
+                // return null is used, because of the if-statement in our Main.java at line 23 and 29.
+                // System.out.println("JUMPSEARCH ARRAY (Checkpoint 1, line 43)");
+                return null;
+            }
+        }
+
+        // apparently this does a Sequential Search
+        while (list[previousPosition].compareTo(object) < 0){
+            previousPosition++;
+
+            if (previousPosition == squareRootOfListLength || previousPosition == list.length){
+                // return null is used, because of the if-statement in our Main.java at line 23 and 29.
+                // System.out.println("JUMPSEARCH ARRAY (Checkpoint 2, line 54)");
+                return null;
+            }
+        }
+
+        // not sure why we need this
+        if (list[previousPosition] == object){
+            // System.out.println("JUMPSEARCH ARRAY (Checkpoint 3, line 61)");
+            return previousPosition;
+        }
+
+        // return null is used, because of the if-statement in our Main.java at line 23 and 29.
+        // System.out.println("JUMPSEARCH ARRAY (Checkpoint 4, line 66)");
         return null;
     }
 
     public static <T extends Comparable<T>> Integer JumpSearch(List<T> list, T object){
+        // calculate the squareroot of the list.length
+        int squareRootOfListLength = (int) Math.sqrt(list.size());
+        // create an integer for the last position jumped to
+        int previousPosition = 0;
+
+        // I have no clue what the fuck is going on here - apparently it's a jumpsearch
+        while (list.get(Math.min(squareRootOfListLength - 1, list.size() - 1)).compareTo(object) < 0){
+            // set the current position to the previous position
+            previousPosition = squareRootOfListLength;
+
+            // basically do square root + square root
+            squareRootOfListLength = squareRootOfListLength + (int) Math.sqrt(list.size());
+
+            // to avoid throwing an exception and ending the program, just return null if previousPosition is equal to
+            // or longer than the length of the list.
+            if (previousPosition >= list.size()){
+                // return null is used, because of the if-statement in our Main.java at line 23 and 29.
+                // System.out.println("JUMPSEARCH LIST (Checkpoint 5, line 88)");
+                return null;
+            }
+        }
+
+        // apparently this does a Sequential Search
+        while (list.get(previousPosition).compareTo(object) < 0){
+            previousPosition++;
+
+            if (previousPosition == squareRootOfListLength || previousPosition == list.size()){
+                // return null is used, because of the if-statement in our Main.java at line 23 and 29.
+                // System.out.println("JUMPSEARCH LIST (Checkpoint 6, line 99)");
+                return null;
+            }
+        }
+
+        // not sure why we need this
+        if (list.get(previousPosition) == object){
+            // System.out.println("JUMPSEARCH LIST (Checkpoint 7, line 105)");
+            return previousPosition;
+        }
+
+        // return null is used, because of the if-statement in our Main.java at line 23 and 29.
+        // System.out.println("JUMPSEARCH LIST (Checkpoint 8, line 111)");
+        return null;
+    }
+
+    // int low = start of the array
+    // int high = length of the array
+    public static <T extends Comparable<T>> Integer BinarySearch(T[] list, T object, int low, int high){
+        if (low > high) {
+            // System.out.println("BINARYSEARCH ARRAY (Checkpoint 9, line 119)");
+            return null;
+        }
+
+        // should be the middle of the array
+        int mid = (low+high) / 2;
+
+        if (list[mid].compareTo(object) == 0){
+            // System.out.println("BINARYSEARCH ARRAY (Checkpoint 10, line 125)");
+            return mid;
+        }
+
+        // if mid is smaller than the object
+        if (list[mid].compareTo(object) < 0){
+            // System.out.println("BINARYSEARCH ARRAY (Checkpoint 11, line 133)");
+            return BinarySearch(list, object, mid + 1, high);
+        }
+
+        // if mid is bigger than the object
+        if (list[mid].compareTo(object) > 0){
+            // System.out.println("BINARYSEARCH ARRAY (Checkpoint 12, line 139)");
+            return BinarySearch(list, object, low, mid - 1);
+        }
+
+        // System.out.println("BINARYSEARCH ARRAY (Checkpoint 13, line 143)");
+        return null;
+    }
+
+    // int low = start of the array
+    // int high = length of the array
+    public static <T extends Comparable<T>> Integer BinarySearch(List<T> list, T object, int low, int high){
+        if (low > high) {
+            // System.out.println("BINARYSEARCH LIST (Checkpoint 14, line 151)");
+            return null;
+        }
+
+        // should be the middle of the array
+        int mid = (low+high) / 2;
+
+        if (list.get(mid).compareTo(object) == 0){
+            // System.out.println("BINARYSEARCH LIST (Checkpoint 15, line 156)");
+            return mid;
+        }
+
+        // if mid is smaller than the object
+        if (list.get(mid).compareTo(object) < 0){
+            // System.out.println("BINARYSEARCH LIST (Checkpoint 16, line 165)");
+            return BinarySearch(list, object, mid + 1, high);
+        }
+
+        // if mid is bigger than the object
+        if (list.get(mid).compareTo(object) > 0){
+            // System.out.println("BINARYSEARCH LIST (Checkpoint 17, line 171)");
+            return BinarySearch(list, object, low, mid - 1);
+        }
+
+        // System.out.println("BINARYSEARCH LIST (Checkpoint 18, line 175)");
         return null;
     }
 
